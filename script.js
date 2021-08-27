b712f2c2fe35ad6a9fef7986e63a38cb71ec2484
Steven Watson <swatson.emu@gmail.com>
2021-08-27 11:30:07 -0500

    Updates on showcase, services, about and pricing. Mooved to Sass for better CSS organization.

--- script.js
+++ script.js
@@ -1,75 +1,18 @@
-
 // Mobile navigaiton fix //
 
 // Closes mobile menu when user clicks nav link
 
-const checkbox = document.getElementById('checkbox');
-const navigationLinks = document.getElementById('mobile-links').querySelectorAll('li');
+const checkbox = document.getElementById("checkbox");
+const navigationLinks = document
+  .getElementById("mobile-links")
+  .querySelectorAll("li");
 
-
-navigationLinks.forEach(listItem => {
-    listItem.addEventListener('click', toggleMenu)
-})
-
+navigationLinks.forEach((listItem) => {
+  listItem.addEventListener("click", toggleMenu);
+});
 
 function toggleMenu() {
-    if(checkbox) {
-        checkbox.checked = false;
-    }
+  if (checkbox) {
+    checkbox.checked = false;
+  }
 }
-
-
-// Scroll Effect //
-
-const serviceScrollItems = document.querySelectorAll('.services-scroll-effect');
-const teamScrollItems = document.querySelectorAll('.team-scroll-effect');
-
-
-window.addEventListener('scroll', handleScrollEffect);
-
-
-function handleScrollEffect() {
-
-    const triggerBottom = window.innerHeight / 5 * 4;
-
-    
-
-    serviceScrollItems.forEach((item, index) => {
-
-        let parentNode = item.parentNode;
-        
-        let delayTime = index + 1;
-        
-        const parentTop = parentNode.getBoundingClientRect().top;
-
-         if(parentTop < triggerBottom) {
-             item.classList.add('show');
-             item.classList.add(`delay-${delayTime}`)
-         } else {
-             item.classList.remove('show');
-             item.classList.remove(`delay-${delayTime}`);
-         }
-        
-    })
-
-
-    teamScrollItems.forEach((item, index) => {
-
-        let parentNode = item.parentNode;
-        
-        let delayTime = index + 1;
-        
-        const parentTop = parentNode.getBoundingClientRect().top;
-
-         if(parentTop < triggerBottom) {
-             item.classList.add('show');
-             item.classList.add(`delay-${delayTime}`)
-         } else {
-             item.classList.remove('show');
-             item.classList.remove(`delay-${delayTime}`);
-         }
-        
-    })
-}
-
-
